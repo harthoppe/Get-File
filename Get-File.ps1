e083f823af433858a8cc9d4c04c68842269fe1c4
hhoppework <hhoppe@ckr.com>
2025-04-03 14:14:46 -0500

    Refactor Get-File function to simplify parameters and improve file download handling

--- Get-File.ps1
+++ Get-File.ps1
@@ -1,5 +1,7 @@
 function Get-File {
+    
     [CmdletBinding()]
+    
     param (
         [Parameter(Mandatory = $true)]
         [string] $source,
@@ -7,25 +9,23 @@ function Get-File {
         [Parameter(Mandatory = $true)]
         [string] $destination,
 
-        [Parameter(Mandatory = $true)]
-        [string] $fileName,
-
-        [Parameter(Mandatory = $true)]
-        [bool] $expandArchive
+        [Parameter(Mandatory = $false)]
+        [bool] $expandArchive = $true
     )
 
-    $outFile = Join-Path -Path $destination -ChildPath $fileName
-
     try {
-        Invoke-WebRequest -Uri $source -OutFile $outFile -ErrorAction Stop
-        Write-Host "File downloaded successfully to $outFile"
-    }
-    catch {
+        Invoke-WebRequest -Uri $source -PassThru -ErrorAction Stop | Tee-Object -Variable response
+        $fileName = $response.headers.'Content-Disposition' -replace '.*filename="([^"]+)".*', '$1'
+        $downloadPath = Join-Path -Path $destination -ChildPath $fileName
+        if (Test-Path -Path $downloadedPath) {
+        Write-Host "File downloaded successfully to $downloadPath"
+        }
+    } catch {
         Write-Host "Failed to download file. Error: $_"
         exit
     }
     
-    if ($expandArchive) {
+    if ($expandArchive -and $fileName -match '\.zip$') {
         try {
             Expand-Archive -LiteralPath $outFile -DestinationPath $destination -Force -ErrorAction Stop
             Write-Host "Archive expanded successfully to $destination"
@@ -37,11 +37,11 @@ function Get-File {
             exit
         }
     }
+
 }
 
 
 $env:source = "https://app.box.com/shared/static/xcuil54m9ptqs2m59t69y4y1dama4nc2.jpg"
 $env:destination = "C:\Temp"
-$env:fileName = "test.zip"
 
-Get-File -source $env:source -destination $env:destination -expandArchive $true -fileName $env:fileName
+Get-File -source $env:source -destination $env:destination -expandArchive $true
